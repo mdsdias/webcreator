8e1bffd8f9ea2f898e6df4b3d9ab3792221193e8
legumii <isqne@pm.me>
2021-12-13 11:04:35 -0300

    Test

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
-app.set('view engine', 'pug');
-app.set('views', './views');
-app.get('/', (req, res) => {
-    res.render('index.pug');
+app.use(express.static("public"));
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.get("/", (req, res) => {
+  res.render("index.pug");
 });
 
-app.listen(3000, () => { console.log('[Server] Started!'); });
+app.listen(3000, () => {
+  console.log("[Server] Started!");
+});
